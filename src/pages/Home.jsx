1004e2becad415ee85ceb736a37816b62737df0c
Philip-Machar <philipmachar788@gmail.com>
2025-06-25 13:26:07 +0300

    version 2 update 2.1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -106,7 +106,7 @@ function Home() {
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h10a2 2 0 012 2v14a2 2 0 01-2 2z" />
                   </svg>
-                  About
+                  About Us
                 </span>
               </button>
               <button
